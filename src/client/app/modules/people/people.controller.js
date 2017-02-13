256ce8d220cd056bbb15f6ca83ea8682bd7026e6
hc3 <eliel.floyd@bol.com.br>
2017-02-13 11:41:05 -0300

    adicionando funcionalidades crud

--- src/client/app/modules/people/people.controller.js
+++ src/client/app/modules/people/people.controller.js
@@ -7,11 +7,13 @@
 		.controller('PeopleControllerList', PeopleControllerList)
 		.controller('PeopleControllerEdit', PeopleControllerEdit)
 		.controller('PeopleControllerView', PeopleControllerView)
+		.controller('PeopleControllerRemove', PeopleControllerRemove)
 
 	PeopleControllerNew.$inject = ['PeopleService', '$state', '$stateParams'];
-	PeopleControllerList.$inject = ['PeopleService', '$state', '$stateParams'];
+	PeopleControllerList.$inject = ['PeopleService', '$state', '$stateParams','$mdDialog'];
 	PeopleControllerEdit.$inject = [];
 	PeopleControllerView.$inject = [];
+	PeopleControllerRemove.$inject = ['$mdDialog','peopleSelected'];
 
 
 	function PeopleControllerNew(PeopleService, $state, stateParams) {
@@ -23,7 +25,7 @@
 		}
 	};
 
-	function PeopleControllerList(PeopleService, $state, stateParams) {
+	function PeopleControllerList(PeopleService, $state, stateParams, $mdDialog) {
 		var vm = this;
 		/* VARIAVEIS BINDING */
 		vm.listPeople = [];
@@ -43,6 +45,8 @@
 
 		vm.listAll = listAll();
 		vm.removeFilter = removeFilter;
+		vm.delete = remove;
+		vm.insert = insert;
 
 		/* FUNÇÕES */
 		listAll();
@@ -62,6 +66,29 @@
 			}
 		};
 
+	  	function remove(event) {
+		    $mdDialog.show({
+		      clickOutsideToClose: true,
+		      controller: 'PeopleControllerRemove',
+		      controllerAs: 'vm',
+		      focusOnOpen: false,
+		      targetEvent: event,
+		      locals: { peopleSelected: vm.selected },
+		      templateUrl: 'app/modules/people/templates/delete-dialog.html',
+		    }).then(vm.listAll);
+	  	};
+
+	  	function insert(event) {
+	  		$mdDialog.show({
+		      clickOutsideToClose: true,
+		      controller: 'PeopleControllerNew',
+		      controllerAs: 'vm',
+		      focusOnOpen: false,
+		      targetEvent: event,
+		      templateUrl: 'app/modules/people/templates/people_new.html',
+		    }).then(vm.listAll);
+	  	}
+
 	};
 
 	function PeopleControllerEdit() {
@@ -70,6 +97,18 @@
 
 	function PeopleControllerView() {
 		return
+	};
+
+	function PeopleControllerRemove($mdDialog,peopleSelected) {
+		var vm = this;
+		vm.cancel = $mdDialog.cancel;
+		
+		vm.removePeople = removePeople;
+
+		function removePeople() {
+			console.log('removendo pessoa',peopleSelected[0].codeInterno);
+		}
+		//console.log('MDDIALOG',people);
 	};
 
 
